a907e809e15b01cbdbc87ef40f7f5f4503a1a942
Mark Furland <markfurland@gmail.com>
2014-01-18 18:49:40 -0500

    inputEmail is now also deals with PDF
    Creates and deletes it.

--- inputEmail.php
+++ inputEmail.php
@@ -5,3 +5,18 @@ $to = $_POST["to"];
 $subject = $_POST["subject"];
 $htmlContent = $_POST["html"];
 
+
+
+require_once("dompdf/dompdf_config.inc.php");
+
+$dompdf = new DOMPDF();
+$dompdf->load_html($subject . $htmlContent);
+$dompdf->render();
+file_put_contents("message.pdf", $dompdf->output( array("compress" => 0) ));
+
+
+//Create message
+
+
+
+unlink("message.pdf");//Delete file
