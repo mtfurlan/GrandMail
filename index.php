d6de2509a47645567a87c55b502a13a5c1514605
Mark Furland <markfurland@gmail.com>
2014-01-18 18:48:33 -0500

    Cleaned up index, and a feature.
    The sub heading is now random.
    Moved the css to a file

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
 <html>
 	<head>
 		<title>GrandMail</title>
-		<style type="text/css">
-		body {
-			text-align: center;
-			background: url('http://thedanyaal.com/img/geometry/geometry.png');
-		}
-		h2 {
-			font-family: Helvetica, Arial, sans-serif;
-			margin-top: 0;
-		}
-		#logo {
-			width: 50%;
-		}
-		.half
-		{
-			display: inline-block;
-			width: 48%;
-		}
-		#description {
-			font-size: 25px;
-			text-align: left;
-			float:right;
-		}
-		</style>
+		<link href="css.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<img id="logo" src="img/grandmail.png">
-		<h2>Because family matters.</h2>
+		<h2><?PHP $f_contents = file("randomHeadings.txt"); echo substr($f_contents[rand(0, count($f_contents) - 1)],0,-1); ?></h2>
 			<div id="description">
 				<div class="half" style="float:left; margin-right:2%;">
 					<img src="img/grandma.jpg" style="width:100%;">
@@ -36,13 +14,5 @@
 					GrandMail facilitates interaction between the youthful and the pre-grave. In a world where communication becomes more and more open between people, there is one segmented audience that is getting shafted by this.
 				</div>
 			</div>
-		<pre>
-			<?php
-				/*
-				 * This is a test file that just prints out the query, for debugging purposes.
-				*/
-				/*print_r($_GET);*/
-			?>
-		</pre>
 	</body>
 </html>
