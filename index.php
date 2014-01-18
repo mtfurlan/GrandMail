cb12696838c8d09472e950d2bc80828d507612fb
Danyaal Rangwala <thedanyaal@gmail.com>
2014-01-18 16:19:08 -0500

    shitty landing up

--- index.php
+++ index.php
@@ -1,14 +1,47 @@
 <html>
 	<head>
-		<title>Test Page</title>
+		<title>GrandMail</title>
+		<style type="text/css">
+		body {
+			text-align: center;
+			background: url('http://thedanyaal.com/img/geometry/geometry.png');
+		}
+		h2 {
+			font-family: Helvetica, Arial, sans-serif;
+			margin-top: 0;
+		}
+		#logo {
+			width: 50%;
+		}
+		.half
+		{
+			display: inline-block;
+			width: 48%;
+		}
+		#description {
+			font-size: 25px;
+			text-align: left;
+			float:right;
+		}
+		</style>
 	</head>
 	<body>
+		<img id="logo" src="img/grandmail.png">
+		<h2>Because family matters.</h2>
+			<div id="description">
+				<div class="half" style="float:left; margin-right:2%;">
+					<img src="img/grandma.jpg" style="width:100%;">
+				</div>
+				<div class="half" id="description">
+					GrandMail facilitates interaction between the youthful and the pre-grave. In a world where communication becomes more and more open between people, there is one segmented audience that is getting shafted by this.
+				</div>
+			</div>
 		<pre>
 			<?php
 				/*
 				 * This is a test file that just prints out the query, for debugging purposes.
 				*/
-				print_r($_GET);
+				/*print_r($_GET);*/
 			?>
 		</pre>
 	</body>
