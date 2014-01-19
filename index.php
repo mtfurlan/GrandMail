5f9e918e548170b34db6be827a75dbb3c652ac38
Mark Furland <markfurland@gmail.com>
2014-01-18 21:28:21 -0500

    One index, can call multiple things

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <html>
 	<head>
-		<title>GrandMail</title>
+		<title>
+			<?PHP 
+				switch((isset($_GET["Page"]) ? $_GET["Page"] : "" )){
+					case "form":
+						echo "Send a tattooed dead tree";
+						break;
+					default:
+						echo "GrandMail";
+				}
+			?>
+			</title>
 		<link href="css.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<img id="logo" src="img/grandmail.png">
+		<a href="?"><img id="logo" src="img/grandmail.png"></a>
 		<h2><?PHP $f_contents = file("randomHeadings.txt"); echo substr($f_contents[rand(0, count($f_contents) - 1)],0,-1); ?></h2>
-			<div id="description">
-				<div class="half" style="float:left; margin-right:2%;">
-					<img src="img/grandma.jpg" style="width:100%;">
-				</div>
-				<div class="half" id="description">
-					GrandMail facilitates interaction between the youthful and the pre-grave. In a world where communication becomes more and more open between people, there is one segmented audience that is getting shafted by this.
-				</div>
-			</div>
+			<article id="main">
+				<?PHP
+					switch((isset($_GET["Page"]) ? $_GET["Page"] : "" )){
+						case "form":
+							include("mailform.php");
+							break;
+						default:
+							echo "GrandMail facilitates interaction between the youthful and the pre-grave. In a world where communication becomes more and more open between people, there is one segmented audience that is getting shafted by this.";
+					}
+				?>
+			</article>
 	</body>
 </html>
