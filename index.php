b98032f7eb0097ac21940858d03759fe4d40924a
Mark Furland <markfurland@gmail.com>
2014-01-19 10:02:23 -0500

    Extra comments, and adding bootstrap CSS.

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ ini_set("display_errors", 1);
 ?>
 <html>
 	<head>
-		<title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+<title>
 			<?PHP 
 				switch((isset($_GET["Page"]) ? $_GET["Page"] : "" )){
 					case "form":
@@ -35,7 +36,8 @@ ini_set("display_errors", 1);
 						default:
 							echo "GrandMail facilitates interaction between the youthful and the pre-grave. " . 
 							"In a world where communication becomes more and more open between people, there is one segmented " .
-							"audience that is getting shafted by this.<br><a href=\"?Page=form\">Form</a><br><a href=\"?Page=db\">Add email option stuff</a>";
+							"audience that is getting shafted by this.The form link is for directly mailing notes.<br>" . 
+"<a href=\"?Page=form\">Form</a><br>The other link is for the actually interesting part, which is how you add addresses you can email. On that form you put the email you would be sending things from in, and put an ID for each address. You then email *ID*@grandmail.bymail.in<br>The subject line is currently not used, but will be used for templates in the future. The body of the email is what is printed out and sent.<a href=\"?Page=db\">Add email option stuff</a><br>Also, special shenanagans allow for multiple people to use the same ID for different addresses. There is no way for you to accidentally send a letter to an address someone else entered.";
 					}
 				?>
 			</article>
